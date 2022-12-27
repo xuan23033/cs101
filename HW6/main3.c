ee5682f25538ca09e40cf91b1db76be18c3b8496
xuan23033 <113535717+xuan23033@users.noreply.github.com>
2022-12-28 00:35:31 +0800

    Add files via upload

--- HW6/main3.c
+++ HW6/main3.c
@@ -3,10 +3,11 @@
 int main() {
     int year = 2022;
     
-    if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0 && year % 4000 != 0))
-        printf("西元 %d 年為閏年。\n", year);
-    else
-        printf("西元 %d 年為平年。\n", year);
-
+    if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0 && year % 4000 != 0)) {
+        printf("\t 西元 %d 年為閏年。\n", year);
+    } else {
+        printf("\t 西元 %d 年不為閏年。\n", year);
+    }
+    
     return 0;
 }
